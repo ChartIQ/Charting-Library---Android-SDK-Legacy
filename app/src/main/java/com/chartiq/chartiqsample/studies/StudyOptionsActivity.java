f0664d1dd5584dd973abdd3d79c57f5533e6816e
Jacob <jacob@chartiq.com>
2019-06-20 11:20:35 -0400

    Simplify block and fix alpha level calculation

--- app/src/main/java/com/chartiq/chartiqsample/studies/StudyOptionsActivity.java
+++ app/src/main/java/com/chartiq/chartiqsample/studies/StudyOptionsActivity.java
@@ -266,13 +266,16 @@ public class StudyOptionsActivity extends AppCompatActivity {
                     String subString = parameter.color.substring(parameter.color.indexOf('(') + 1, parameter.color.indexOf(')'));
                     String rgbColors[] = subString.split(",");
                     int parsedColor = 0;
-                    if(rgbColors.length == 4) { // contains an alpha value
-                        parsedColor = Color.argb(Integer.parseInt(rgbColors[3].trim()), Integer.parseInt(rgbColors[0].trim()),
-                                Integer.parseInt(rgbColors[1].trim()), Integer.parseInt(rgbColors[2].trim()));
-                    } else {
-                        parsedColor = Color.rgb(Integer.parseInt(rgbColors[0].trim()),
-                                Integer.parseInt(rgbColors[1].trim()), Integer.parseInt(rgbColors[2].trim()));
+                    int alphaValue = 255;
+
+                    // contains an alpha value
+                    if(rgbColors.length == 4) {
+                        double value = Double.parseDouble(rgbColors[3].trim());
+                        value = Math.floor(value >= 1.0 ? 255 : value * 256.0); // use 256 for floating point precision when value is less than 1.0
+                        alphaValue = (int) value;
                     }
+                    parsedColor = Color.argb(alphaValue, Integer.parseInt(rgbColors[0].trim()),
+                            Integer.parseInt(rgbColors[1].trim()), Integer.parseInt(rgbColors[2].trim()));
 
                     color.setBackgroundColor(parsedColor);
                 } else {
